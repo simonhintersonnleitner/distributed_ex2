4711984bfeaf0ef5f0707eaca40332ff036ad1c3
Simon Hintersonnleitner <shintersonnleitner.mmt-b2013@fh-salzburg.ac.at>
2015-05-19 22:50:34 +0200

    add regsiter button

--- client.js
+++ client.js
@@ -1,11 +1,23 @@
 var socket = io.connect('http://localhost', {path: "/public/socket.io"});
 
 $(document).ready(function (){
-	$('#login').click(function(){
-    socket.emit('login', $('#id').val(),$('#pw').val());
+  $('#login').click(function(){
+  socket.emit('login', $('#id').val(),$('#pw').val());
   });
-  socket.on('login_result', function(user,pw){
+  
+  socket.on('login_result', function(user){
+    console.log("Test");
+  });
+
+  $('#register').click(function(){
+  console.log("Register");
+  socket.emit('register', $('#id').val(),$('#pw').val());
+  });
+
+  socket.on('register_result', function(user){
     console.log("Test");
   });
 });
+
+
   
--- public/client.js
+++ public/client.js
@@ -1,16 +1,33 @@
-
-
-//var socket = io.connect('http://localhost', { resource: 'public/socket.io' });
 var socket = io.connect('http://localhost', {path: "/public/socket.io"});
-//var socket = io();
-
 
 $(document).ready(function (){
-	$('#login').click(function(){
-    socket.emit('login', $('#id').val(),$('#pw').val());
+  $('#login').click(function(){
+  socket.emit('login', $('#user').val(),$('#pw').val());
   });
-  socket.on('login_result', function(user,pw){
-    console.log("Test");
+  
+  socket.on('login_result', function(res){
+    console.log("login_result " + res);
+    $('#output').empty();
+    if(res == 1)
+    	$('#output').append("Login erflogreich!");
+   	else
+   		$('#output').append("<p>Login nicht erflogreich!</p>");
+  });
+
+  $('#register').click(function(){
+  console.log("Register");
+  socket.emit('register', $('#user').val(),$('#pw').val());
+  });
+
+  socket.on('register_result', function(res){
+    console.log("register_result " + res);
+    $('#output').empty();
+    if(res == 1)
+      $('#output').append("Registierung erflogreich!");
+    else
+      $('#output').append("<p>Registierung nicht erflogreich!</p>");
   });
 });
+
+
   
